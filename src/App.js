33da745f60350bc5d145c8c747a6873f03ec8d1e
moriah <snailans73@gmail.com>
2023-05-25 11:57:14 +0600

    re commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import { TeamPage } from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
+import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/teams/:teamName/matches/:year">
+            <MatchPage />
+          </Route>
+          <Route path="/teams/:teamName">
+            <TeamPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
